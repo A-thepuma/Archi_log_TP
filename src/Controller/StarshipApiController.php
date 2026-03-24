ff6392a9ab4693636f083b9c9da1e7fc07686928
André <111359909+A-thepuma@users.noreply.github.com>
2026-03-24 17:09:51 +0100

    Création d'une API JSON

---
+++ src/Controller/StarshipApiController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Starship;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+class StarshipApiController extends AbstractController
+{
+    #[Route('/api/starships')]
+    public function getCollection() : Response
+    {
+        $starships = [
+            new Starship(
+                1,
+                 'USS LeafyCruiser (NCC-0001)',
+                 'Garden',
+                 'Jean-Luc Pickles',
+                 'taken over by Q',
+            ),
+            new Starship(
+                 2,
+                'USS WanderLust (WCC-0002)',
+                 'Latte',
+                 'Jean-Luc snake',
+                 ' over by Q',
+            ),
+
+            new Starship(
+                3,
+                'USS Expresso (WCC-0002)',
+                'aa',
+                'Jean-Luc canica',
+                '  by Q',
+            ),
+            ];
+
+        return $this->json($starships);
+    }
+}
